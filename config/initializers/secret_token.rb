7691199e253531f800d1ea146224af998f588891
James Johnston <james@happysquare.com.au>
2011-01-20 16:02:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7fd1b98d44f473b8c9524ba209c42d15cf72747fef3ddf118ab7605714fb7513ae50ad0b2bd23aadde037d36cb88dc17409787263328b11e35f34bff91c72d54'
